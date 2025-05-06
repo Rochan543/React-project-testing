73c48ad1a069ddb30feced42d41db59e36d5454f
Rochan543 <premrochan@gmail.com>
2025-05-06 15:39:00 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My React Website</h1>
+      <p>This site is deployed on Azure App Service 🚀</p>
+    </div>
+  );
+}
+
+export default App;
